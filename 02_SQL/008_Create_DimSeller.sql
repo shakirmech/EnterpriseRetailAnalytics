6f5f9a3ab306db67c341ab365b508f9a548ba404
shakir <shakirmech@gmail.com>
2026-07-05 07:48:19 +0530

    Created DimDate Table

--- 02_SQL/008_Create_DimSeller.sql
+++ 02_SQL/008_Create_DimSeller.sql
@@ -1,2 +1,25 @@
 USE ENTERPRISERETAILDW;
 GO
+
+IF OBJECT_ID('dbo.Dimseller', 'U') IS NOT NULL
+DROP TABLE dbo.Dimseller;
+GO
+
+CREATE TABLE dbo.Dimseller
+(
+	sellerKey INT IDENTITY(1,1) NOT NULL
+		CONSTRAINT PK_Dimseller
+		PRIMARY KEY (sellerKey),
+	sellerID VARCHAR(50) NOT NULL
+		CONSTRAINT UQ_Dimseller_sellerID
+		UNIQUE (sellerID),
+	sellerzipcodeprefix INT NULL,
+	sellercity VARCHAR(100) NULL,
+	sellerstate CHAR(2) NULL,
+	loadDate DATETIME NOT NULL
+		CONSTRAINT DF_Dimseller_loadDate
+		DEFAULT GETDATE()
+);
+GO
+
+
---
+++ 02_SQL/011_Create_DimDate.sql
@@ -0,0 +1,23 @@
+USE EnterpriseRetailDW;
+GO
+
+IF OBJECT_ID('dbo.DimDate', 'U') IS NOT NULL
+    DROP TABLE dbo.DimDate;
+GO
+
+CREATE TABLE dbo.DimDate
+(
+    DateKey INT IDENTITY(1,1) NOT NULL,
+        CONSTRAINT PK_DimDate
+        PRIMARY KEY (DateKey)
+    FullDate DATE NOT NULL,
+    DayNumber TINYINT NOT NULL,
+    MonthNumber  TINYINT NOT NULL,
+    MonthName VARCHAR(20) NOT NULL,
+    QuarterNumber TINYINT NOT NULL,
+    YearNumber SMALLINT NOT NULL,
+    WeekNumber TINYINT NOT NULL,
+    DayName VARCHAR(20) NOT NULL,
+    IsWeekend BIT NOT NULL
+);
+GO

b5e357d48787792016c8cdd0098241dc7af0f94c
shakir <shakirmech@gmail.com>
2026-07-05 06:32:51 +0530

    Loaded Dimseller Dimension

--- 02_SQL/009_Load_DimSeller.sql
+++ 02_SQL/009_Load_DimSeller.sql
@@ -1,2 +1,21 @@
 USE EnterpriseRetailDW;
 GO
+
+INSERT INTO dbo.Dimseller
+(
+    sellerID,
+    sellerzipcodeprefix,
+    sellercity,
+    sellerstate
+)
+SELECT 
+    seller_id,
+    seller_zip_code_prefix,
+    seller_city,
+    seller_state
+FROM stg.Sellers;
+GO
+
+select top(10)* from Dimseller
+
+select COUNT(*) from Dimseller
